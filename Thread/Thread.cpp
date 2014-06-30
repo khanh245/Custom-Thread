9a922c519c09d72d6d1de6441d19459db6a6029c
Khanh Nguyen <Khanh@M15x.com>
2014-06-29 23:30:41 -0700

    Added thread class

---
+++ Thread/Thread.cpp
@@ -0,0 +1,10 @@
+/*
+ * Thread.cpp
+ *
+ *  Created on: Jun 29, 2014
+ *      Author: khanhn
+ */
+
+#include "Thread.h"
+
+
---
+++ Thread/Thread.h
@@ -0,0 +1,23 @@
+/*
+ * Thread.h
+ *
+ *  Created on: Jun 29, 2014
+ *      Author: khanhn
+ */
+
+#ifndef THREAD_H_
+#define THREAD_H_
+
+class Thread
+{
+public:
+
+protected:
+
+private:
+
+};
+
+
+
+#endif /* THREAD_H_ */
