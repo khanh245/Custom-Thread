eb14c9ad47774fd382d8238980207835276c096a
Khanh Nguyen <Khanh@M15x.com>
2014-06-29 23:27:50 -0700

    - Added driver file for custom thread

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jun 29, 2014
+ *      Author: khanhn
+ *
+ *  Purpose: Custom Thread in C++ Driver
+ */
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "This is a driver for custom thread. This is just a POC and will be used in Kronos." << std::endl;
+
+	return 0;
+}
